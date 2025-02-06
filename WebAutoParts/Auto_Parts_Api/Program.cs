f8229fac563bcdb4661ea244672ba058f92326db
MIRADA <vitaliy.laben@gmail.com>
2025-02-06 17:16:23 +0200

    Add ASP.Net Web API

---
+++ WebAutoParts/Auto_Parts_Api/Program.cs
@@ -0,0 +1,48 @@
+using Core;
+using Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
+
+
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext(connectionString);
+
+builder.Services.AddMapper();
+
+builder.Services.AddServices();
+builder.Services.AddFluentValidators();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.MapOpenApi();
+//}
+app.UseCors(options =>
+{
+    options.AllowAnyHeader();
+    options.AllowAnyMethod();
+    options.WithOrigins("http://localhost:5173");
+});
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ WebAutoParts/Core/ServiceExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interfaces;
+using Core.MapperProfile;
+using Core.Services;
+using FluentValidation.AspNetCore;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core
+{
+    public static class ServiceExtensions
+    {
+        public static void AddMapper(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(AppProfile));
+        }
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IPartsService,SparePartService>();
+        }
+        public static void AddFluentValidators(this IServiceCollection services)
+        {
+            services.AddFluentValidationAutoValidation();
+            // enable client-side validation
+            services.AddFluentValidationClientsideAdapters();
+            // Load an assembly reference rather than using a marker type.
+            services.AddValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
+        }
+
+    }
+}
